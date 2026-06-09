b07212bf8d1e7ba7fadb2d0ae9bad93b7f7ceb22
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-06-09 13:19:20 +0200

    added docker files

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+set -e # Crash if any commands fails
+
+# Ensure socket directory exists and is owned by mysql
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+# Check for existing internal system tables
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+    # Create a temp file to hold initialization SQL commands
+    echo "Creating temporary SQL bootstrap file..."
+    cat << EOF > /tmp/init.sql
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+    # Run commands internally without starting the network engine
+    echo "Running bootstrap configuration..."
+    mariadbd --user=mysql --bootstrap < /tmp/init.sql
+    rm -f /tmp/init.sql
+fi
+
+# Run mariadbd in foreground
+exec mariadbd --user=mysql
---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env bash
+set -e # Crash if any commands fails
+
+# Ensure PHP-FPM runtime directory exists
+mkdir -p /run/php
+
+# Check if WordPress is already downloaded
+if [ ! -f "wp-config.php" ]; then
+    echo "Downloading WordPress..."
+    wp core download --allow-root
+
+echo "Creating 'wp-config.php'..."
+    wp config create \
+        --dbname="${MYSQL_DATABASE}" \
+        --dbuser="${MYSQL_USER}" \
+        --dbpass="${MYSQL_PASSWORD}" \
+        --dbhost="mariadb" \
+        --allow-root
+
+    echo "Installing WordPress..."
+    wp core install \
+        --url="https://${DOMAIN_NAME}" \
+        --title="Inception" \
+        --admin_user="${WP_ADMIN_USER}" \
+        --admin_password="${WP_ADMIN_PASSWORD}" \
+        --admin_email="${WP_ADMIN_EMAIL}" \
+        --skip-email \
+        --allow-root
+
+    echo "Creating regular user..."
+    wp user create "${WP_USER}" "${WP_EMAIL}" \
+        --user_pass="${WP_PASSWORD}" \
+        --role=author \
+        --allow-root
+fi
+
+# Run PHP-FPM in foreground
+exec php-fpm8.2 -F
